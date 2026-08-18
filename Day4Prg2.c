7f024c57f82d577936459f5da08da804244c094e
krishnaaarwal22 <krishnaaarwal22@gmail.com>
2026-08-18 22:52:04 +0530

    Add files via upload

---
+++ Day4Prg2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n;
+int sum;
+printf("Enter the number :");
+scanf("%d",&n);
+sum=n*(n+1)/2;
+printf("The sum of first %d natural number is %d",n,sum);
+
+    return 0;
+}
