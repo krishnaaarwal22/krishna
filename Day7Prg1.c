e3ae763e361dc94cf494788c9b5ae01ba1dbc6b5
krishnaaarwal22 <krishnaaarwal22@gmail.com>
2026-08-18 23:14:33 +0530

    Add files via upload

---
+++ Day7Prg1.c
@@ -0,0 +1,17 @@
+//Write a program to input a year and check whether it is a leap year or not using conditional statements.
+#include<stdio.h>
+int main()
+{
+int year;
+printf("Enter the year number ;");
+scanf("%d",&year);
+if(year%4==0)
+{
+    printf("Leap year");
+}
+else{
+    printf("Not a leap year");
+}
+
+    return 0;
+}
