10ab4d4aba1836a6c140d42ef99c2946b97dc5f0
krishnaaarwal22 <krishnaaarwal22@gmail.com>
2026-09-17 20:45:55 +0530

    Add files via upload

---
+++ Day39Prg1.c
@@ -0,0 +1,45 @@
+// Check if the elements on the diagonal of a matrix are distinct.
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, distinct = 1;
+
+    printf("Enter the size of matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    printf("Enter matrix elements:\n");
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Check diagonal elements
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i][i] == a[j][j])
+            {
+                distinct = 0;
+                break;
+            }
+        }
+
+        if (distinct == 0)
+            break;
+    }
+
+    if (distinct == 1)
+        printf("Diagonal elements are distinct.\n");
+    else
+        printf("Diagonal elements are not distinct.\n");
+
+    return 0;
+}
