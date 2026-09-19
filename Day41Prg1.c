cc738dff62b8de3c81d806b418bc97be1cd28ba8
krishnaaarwal22 <krishnaaarwal22@gmail.com>
2026-09-19 20:31:27 +0530

    Add files via upload

---
+++ Day41Prg1.c
@@ -0,0 +1,21 @@
+//Count characters in a string without using built-in length functions.
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Count characters until null character or newline
+    while (str[count] != '\0' && str[count] != '\n')
+    {
+        count++;
+    }
+
+    printf("Number of characters = %d\n", count);
+
+    return 0;
+}
